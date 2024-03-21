0d7ed173d103986717842328c5c80c97151e56f7
jhorel123 <jhorelrolandoa@gmail.com>
2024-03-21 14:41:42 -0400

    cadena de Conexion

--- Coling/Coling.API.Curriculum/Endpoints/InstitucionFunction.cs
+++ Coling/Coling.API.Curriculum/Endpoints/InstitucionFunction.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Extensions.Logging;
 using System.Net;
 
